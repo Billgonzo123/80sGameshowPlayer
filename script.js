615f4e7e83d74fd18060df567dac46aac408a3b7
CKURZ <ckurz098@gmail.com>
2021-12-06 11:32:52 -0800

    added feature that makes sure no episonde is repeated until all episodes have been displayed

--- script.js
+++ script.js
@@ -24,9 +24,8 @@ if (!localStorage.getItem('channelNum10171615')) { localStorage.setItem('channel
 ///volume and other data saved here
 if (!localStorage.getItem('channelNum10171999')) { localStorage.setItem('channelNum10171999', vol); };
 ///check if there is saved page Data
-if (!localStorage.getItem('channelNum999888777')) { localStorage.setItem('channelNum999888777', JSON.stringify(pageData)); };
 vol = localStorage.getItem('channelNum10171999');
-pageData = JSON.parse(localStorage.getItem('channelNum999888777'));
+
 ///need to do this for a glitch. have to run down first or up will be glitched
 volumeDown();
 volumeUp();
@@ -53,40 +52,45 @@ for (i = 15; i < channel.length; i++) {
 let beginPlace = 0;
 //channel number saved here. Get it
 let num = localStorage.getItem('channelNum10171615');
-////////////////////////////////////////////////////////////////////////////////////
+
+
+//--------------------------------GENERATE RANDOM CHANNEL TO PLAY----------------------------------------------//
 
 ///find random episode
 let rndEpisodeNum = Math.floor((Math.random() * channel[num].episodes) + 1);
 
-//--------------------------------CHECK IF RANDOM CH HAS BEEN GENERATED BEFORE FOR THIS SESSION-----------------------------------
+//--------------------------------CHECK IF RANDOM CHANNEL HAS BEEN GENERATED-----------------------------------//
 
-//if channel was changed on refresh, clear channels form pageData. 
-//if not check if the rndEpisodeNum has been generated before
-if (pageData[0] != num) {
-    pageData = [num];
-    localStorage.setItem('channelNum999888777', JSON.stringify(pageData));
-}
-else {
-    for (let i = 1; i < pageData.length; i++) {
-        if (pageData[i] === rndEpisodeNum) {
-            rndEpisodeNum = Math.floor((Math.random() * channel[num].episodes) + 1);
-            //start for loop over
-            i = 0;
-        }
-        
+/*This checks if the random channel has been generated before, and if it has it finds a new number.
+If there are no more random numbers to select, it clears the array of prev channels and starts over */
 
+//load saved prev generated channels. But first check if one exists. If it dosn't, create an empty one
+if (!localStorage.getItem(num)) { localStorage.setItem(num, JSON.stringify([num])); };
+
+pageData = JSON.parse(localStorage.getItem(num));
+
+//check each prev ch with the current generated ch
+for (let i = 1; i < pageData.length; i++) {
+    //if the ch has been generated before, make new number and start over (i=0)
+    if (pageData[i] === rndEpisodeNum) {
+        rndEpisodeNum = Math.floor((Math.random() * channel[num].episodes) + 1);
+        //start for loop over
+        i = 0;
     }
-  
-    pageData.push(rndEpisodeNum);
-    localStorage.setItem('channelNum999888777', JSON.stringify(pageData));
 }
+  
+///once new ch is found, push it into the array
+    pageData.push(rndEpisodeNum);
+///and save the array to local storage (each channel gets its own local storage slot)
+    localStorage.setItem(num, JSON.stringify(pageData));
 
+//checks if rndEpisodeNum is the last possible number and resets the array if it is
 if (pageData.length > channel[num].episodes)
 {
     pageData = [num];
-    localStorage.setItem('channelNum999888777', JSON.stringify(pageData));
+    localStorage.setItem(num, JSON.stringify(pageData));
 }
-/////////////////////////////////////////////////////////////////////////////
+///////////////////////END RANDOM NUM CHECK//////////////////////////////////////////////////////
 
 //display ch name on screen
 chDisp.textContent = channel[num].name;
@@ -313,7 +317,7 @@ function loadChannels() {
     { name: "Ch: 14 - 80's TV", list: 'PLGS5pi29Z6qFmRfb4q9RPURwKK0xMk6IT', episodes: 507, randPoint: 0 },
     { name: "Ch: 15 - 90's TV", list: 'PLo6LMGdjaTzLxpgNhSiCjzwMPXRLJjwaJ', episodes: 285, randPoint: 0 },
     { name: 'Ch: 16 - Computer Chronicles', list: 'PLmM8tWTshxQBws_fIdi5qH63rZxrlB0qL', episodes: 647, randPoint: 0 },
-    { name: 'Ch: 17 - TechTV', list: 'PLo6LMGdjaTzKuVaftTtnSPfMOOlFhORm8', episodes: 1, randPoint: 1 },
+    { name: 'Ch: 17 - TechTV', list: 'PLo6LMGdjaTzKuVaftTtnSPfMOOlFhORm8', episodes: 1, randPoint: 1 }, //no longer works
     { name: 'Ch: 18 - Xplay', list: 'PLKE9oP_rYnRdLhG3HW__6ytTwvgSBM9pu', episodes: 647, randPoint: 0 },
     { name: 'Ch: 19 - Sitcoms Make Me Cring', list: 'PLGk6y7qjGXVt-tH7E0P2CEedDGJp-fT1p', episodes: 463, randPoint: 0 },
     { name: 'Ch: 20 - MTV', list: 'PLId5xJ_xHV-k3ZgNju2ifMLct7-8uRKr8', episodes: 589, randPoint: 0 },
