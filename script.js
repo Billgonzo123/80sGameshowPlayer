503b5d6797f31d143b5eb792db99c7f6d6c0ab49
CKURZ <ckurz098@gmail.com>
2021-12-05 20:31:00 -0800

    disabled window scroll

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ function onYouTubeIframeAPIReady() {
                 //update vol every 100 ms
                 setInterval(function () { 
                     event.target.setVolume(vol); 
-                    sndSrc.volume= (vol/100);
+                    
                 }, 100)
                 //event.target.cuePlaylist({list: "PLFgquLnL59anYA8FwzqNFMp3KMcbKwMaT"});
                 //event.target.playVideo();
