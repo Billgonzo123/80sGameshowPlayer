4b00a4e0b747317011a2c1766bf81515512f9ed8
CKURZ <ckurz098@gmail.com>
2021-12-06 10:43:47 -0800

    added check for repeated random ep

--- script.js
+++ script.js
@@ -17,14 +17,22 @@ listDisplay2.style.display = 'none';
 const chDisp = document.getElementById('channelNameDisplay');
 chDisp.style.display = "block";
 
+///this will hold all of the channels to avoid repeatson refresh
+let pageData = []
+///clast ch saved here
 if (!localStorage.getItem('channelNum10171615')) { localStorage.setItem('channelNum10171615', 0); };
+///volume and other data saved here
 if (!localStorage.getItem('channelNum10171999')) { localStorage.setItem('channelNum10171999', vol); };
+///check if there is saved page Data
+if (!localStorage.getItem('channelNum999888777')) { localStorage.setItem('channelNum999888777', JSON.stringify(pageData)); };
 vol = localStorage.getItem('channelNum10171999');
+pageData = JSON.parse(localStorage.getItem('channelNum999888777'));
 ///need to do this for a glitch. have to run down first or up will be glitched
 volumeDown();
+volumeUp();
 
-sound.volume= (vol/100);
-  
+sound.volume = (vol / 100);
+
 channelEntry.textContent = "";
 //create array of channels
 //randPoint specifies if a video collection has long multi episode videos. mod is how many 15min sections in those videos (hrs*4)
@@ -43,11 +51,42 @@ for (i = 15; i < channel.length; i++) {
 ///////////////////////////////
 
 let beginPlace = 0;
+//channel number saved here. Get it
 let num = localStorage.getItem('channelNum10171615');
 ////////////////////////////////////////////////////////////////////////////////////
 
-
+///find random episode
 let rndEpisodeNum = Math.floor((Math.random() * channel[num].episodes) + 1);
+
+//--------------------------------CHECK IF RANDOM CH HAS BEEN GENERATED BEFORE FOR THIS SESSION-----------------------------------
+
+//if channel was changed on refresh, clear channels form pageData. 
+//if not check if the rndEpisodeNum has been generated before
+if (pageData[0] != num) {
+    pageData = [num];
+    localStorage.setItem('channelNum999888777', JSON.stringify(pageData));
+}
+else {
+    for (let i = 1; i < pageData.length; i++) {
+        if (pageData[i] === rndEpisodeNum) {
+            rndEpisodeNum = Math.floor((Math.random() * channel[num].episodes) + 1);
+            //start for loop over
+            i = 0;
+        }
+        
+
+    }
+  
+    pageData.push(rndEpisodeNum);
+    localStorage.setItem('channelNum999888777', JSON.stringify(pageData));
+}
+
+if (pageData.length > channel[num].episodes)
+{
+    pageData = [num];
+    localStorage.setItem('channelNum999888777', JSON.stringify(pageData));
+}
+/////////////////////////////////////////////////////////////////////////////
 
 //display ch name on screen
 chDisp.textContent = channel[num].name;
@@ -82,12 +121,11 @@ function onYouTubeIframeAPIReady() {
         events: {
             'onReady': function (event) {
                 //update vol every 100 ms
-                setInterval(function () { 
-                    event.target.setVolume(vol); 
-                    
+                setInterval(function () {
+                    event.target.setVolume(vol);
+
                 }, 100)
-                //event.target.cuePlaylist({list: "PLFgquLnL59anYA8FwzqNFMp3KMcbKwMaT"});
-                //event.target.playVideo();
+
                 setTimeout(function () {
 
                     event.target.setShuffle({ 'shufflePlaylist': true });
@@ -149,7 +187,7 @@ let element = document.addEventListener('keydown', function (event) {
             case '-': if (num <= 0) { num = channel.length - 1; refresh(); } else { num--; refresh(); }
                 break;
             case '.': if (listDisplay.style.display === "none") { chDisp.style.display = "block"; listDisplay.style.display = 'block'; listDisplay2.style.display = 'block'; }
-            else {chDisp.style.display = "none"; listDisplay.style.display = 'none'; listDisplay2.style.display = 'none'; }
+            else { chDisp.style.display = "none"; listDisplay.style.display = 'none'; listDisplay2.style.display = 'none'; }
                 break;
             case '*': volumeUp();
                 break;
@@ -211,12 +249,12 @@ function volumeUp() {
     }
     volEl.textContent += "]";
 
-    sndSrc.volume= (vol/100);
-    console.log("static: ",sndSrc.volume);
+    sndSrc.volume = (vol / 100);
+    console.log("static: ", sndSrc.volume);
 
     localStorage.setItem('channelNum10171999', vol);
     hideVol();
-return;
+    return;
 }
 
 function volumeDown() {
@@ -234,12 +272,12 @@ function volumeDown() {
     }
     volEl.textContent += "]";
 
-    sndSrc.volume= (vol/100);
-    console.log("static: ",sndSrc.volume);
+    sndSrc.volume = (vol / 100);
+    console.log("static: ", sndSrc.volume);
 
     localStorage.setItem('channelNum10171999', vol);
     hideVol();
-return;
+    return;
 }
 
 
@@ -273,7 +311,7 @@ function loadChannels() {
     { name: 'Ch: 12 - Pepper Ann', list: 'PLLhOnau-tupR82ubLjcY2tQNlUMGTn__z', episodes: 160, randPoint: 0 },
     { name: 'Ch: 13 - Loony Tunes', list: 'PLZs0gQed9tMStrGoR2C58YOgQ-HclszH9', episodes: 155, randPoint: 0 },
     { name: "Ch: 14 - 80's TV", list: 'PLGS5pi29Z6qFmRfb4q9RPURwKK0xMk6IT', episodes: 507, randPoint: 0 },
-    { name: "Ch: 15 - 90's TV", list: 'PLo6LMGdjaTzLxpgNhSiCjzwMPXRLJjwaJ', episodes: 285  , randPoint: 0 },
+    { name: "Ch: 15 - 90's TV", list: 'PLo6LMGdjaTzLxpgNhSiCjzwMPXRLJjwaJ', episodes: 285, randPoint: 0 },
     { name: 'Ch: 16 - Computer Chronicles', list: 'PLmM8tWTshxQBws_fIdi5qH63rZxrlB0qL', episodes: 647, randPoint: 0 },
     { name: 'Ch: 17 - TechTV', list: 'PLo6LMGdjaTzKuVaftTtnSPfMOOlFhORm8', episodes: 1, randPoint: 1 },
     { name: 'Ch: 18 - Xplay', list: 'PLKE9oP_rYnRdLhG3HW__6ytTwvgSBM9pu', episodes: 647, randPoint: 0 },
@@ -281,7 +319,7 @@ function loadChannels() {
     { name: 'Ch: 20 - MTV', list: 'PLId5xJ_xHV-k3ZgNju2ifMLct7-8uRKr8', episodes: 589, randPoint: 0 },
     { name: 'Ch: 21 - MST 3000', list: 'PLDXsAHvr3XNPn8PfqYpU7NBHWOzdow89l', episodes: 177, randPoint: 0 },
     { name: 'Ch: 22 - Movies', list: 'PLKxdKKLx3iRTyfWK8SQghHUGHfOTGhRl2', episodes: 914, randPoint: 0 },
-    
+
     ];
     return array;
 }
