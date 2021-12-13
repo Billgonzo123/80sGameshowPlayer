18d70ac17cea1d1354d2d27b34f2e8e9bcd1cbbd
CKURZ <ckurz098@gmail.com>
2021-12-13 15:55:03 -0800

    Fixed video showing  on Overscan. Added new vertical shift feature

--- script.js
+++ script.js
@@ -282,6 +282,7 @@ let element = document.addEventListener('keydown', function (event) {
                 break;
             case '.':
             case ',':
+            case 'Backspace':
                 if (listDisplay.style.display === "none") { chDisp.style.display = "block"; listDisplay.style.display = 'block'; listDisplay2.style.display = 'block'; }
                 else { chDisp.style.display = "none"; listDisplay.style.display = 'none'; listDisplay2.style.display = 'none'; }
                 break;
